74ce38f8f49fecd18395531ba16cc9b4835dd774
qzhou7 <bamboosdu@gmail.com>
2026-03-22 15:04:28 -0400

    update

--- mimic_videos/backbone/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Backbone and feature extractor subpackage."""
-from .cosmos_backbone import CosmosBackbone
-from .feature_extractor import FeatureExtractor, PoolingMode
-
-__all__ = ["CosmosBackbone", "FeatureExtractor", "PoolingMode"]
--- mimic_videos/engine/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Engine subpackage."""
-from .stage import TrainingStage, Stage1Strategy, Stage2Strategy, StageConfig
-from .trainer import Trainer
-
-__all__ = ["TrainingStage", "Stage1Strategy", "Stage2Strategy", "StageConfig", "Trainer"]
